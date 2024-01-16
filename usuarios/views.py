0410b3498d14cc24be49973ddb2e2aa95ac26678
pauloAlves98 <alvespaulo737@gmail.com>
2024-01-16 17:27:00 -0300

    Aula 1: PyStack 9.0: Criar apps (usuarios e flashcard); View: Cadastro e Login

---
+++ usuarios/views.py
@@ -0,0 +1,70 @@
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.messages import constants
+from django.contrib import messages, auth
+
+def cadastro(request):
+    if request.method == 'GET':
+        return render(request, 'cadastro.html')
+    else:
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not senha == confirmar_senha:
+            messages.add_message(
+                request, constants.ERROR, 'As senhas não coincídem'
+            )
+            return redirect(reverse('cadastro'))
+
+        user = User.objects.filter(username=username)
+
+        if user.exists(): #existe algum dado aqui dentro
+            messages.add_message(
+                request,
+                constants.ERROR,
+                'Já existe um usuário com o mesmo username',
+            )
+            return redirect(reverse('cadastro'))
+        try:
+            user = User.objects.create_user(
+                   username=username,
+                   password=confirmar_senha,)
+            #configure as constants no settings
+            messages.add_message(
+                request, constants.ERROR, 'Usuário cadastrado com sucesso.'
+            )
+            return redirect('/usuarios/login')
+        except:
+            messages.add_message(
+                request, constants.ERROR, 'Erro interno do sistema'
+            )
+            return redirect('/usuarios/cadastro')
+
+def logar(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        #autenticação django padrão com username
+        user = auth.authenticate(request, username=username, password=senha)
+        if user:
+            auth.login(request, user)
+            messages.add_message(request, constants.SUCCESS, 'Logado!')
+            return redirect('/flashcard/novo_flashcard/')
+        else:
+            messages.add_message(
+                request, constants.ERROR, 'Username ou senha inválidos'
+            )
+            return redirect('/usuarios/login')
+
+def logout(request):
+    auth.logout(request)
+    return redirect(reverse('login'))
+
+
+
+
